41be84e6a7bd863ad84abad32d6029eba0b09f86
Karasiq69 <128066037+Karasiq69@users.noreply.github.com>
2024-03-21 15:51:25 +0300

    Changed next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,22 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
-    // images: {
-    //   domains: [process.env.NEXT_PUBLIC_HOST?.replace(/^https?:\/\//, '') || 'localhost'],
-    // },
-    // distDir: 'build',
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: '192.168.1.25',
-            },
-            {
                 protocol: 'https',
-                hostname: process.env.NEXT_PUBLIC_HOST?.replace(/^https?:\/\//, ''),
+                hostname: 'api.zharim-varim.top',
+                port: '',
+                pathname: '/**',
             },
             {
                 protocol: 'http',
                 hostname: 'localhost',
                 port: '8000',
+                pathname: '/**',
             },
         ],
-
-
     },
-
 };
 
 export default nextConfig;
-
-
-
