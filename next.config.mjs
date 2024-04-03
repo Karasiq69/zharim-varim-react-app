3d8c7e491f74926f7263f4f5b78f4089507b9328
Karasiq69 <128066037+Karasiq69@users.noreply.github.com>
2024-04-03 16:10:05 +0300

    Fixing env v5

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'api.zharim-varim.top',
+    //             port: '',
+    //             pathname: '/**',
+    //         },
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'api.zharim-cafe.ru',
+    //             port: '',
+    //             pathname: '/**',
+    //         },
+    //         {
+    //             protocol: 'http',
+    //             hostname: 'localhost',
+    //             port: '8000',
+    //             pathname: '/**',
+    //         },
+    //     ],
+    // },
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.zharim-varim.top',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'api.zharim-cafe.ru',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '8000',
-                pathname: '/**',
-            },
-        ],
+        domains: ['localhost', 'zharim-cafe.ru', 'api.zharim-cafe.ru']
     },
 };
 
