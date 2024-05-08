7558765568f71e167f6b67f667ae5cc9934ed8b0
Hedgehogly <0982646@mail.ru>
2024-05-08 22:20:54 +0300

    Fixed social login, added suspense

--- src/app/auth/yandex/page.tsx
+++ src/app/auth/yandex/page.tsx
@@ -2,15 +2,32 @@
 
 import { useSocialAuthenticateMutation } from '@/redux/features/authApiSlice';
 import useSocialAuth from "@/hooks/use-social-auth";
-import {Loader} from "lucide-react";
-// import { useSocialAuth } from '@/hooks';
-// import { Spinner } from '@/components/common';
+import { Loader } from "lucide-react";
+import { Suspense } from 'react';
 
 export default function Page() {
-	const [yandexAuthenticate] = useSocialAuthenticateMutation();
-	useSocialAuth(yandexAuthenticate, 'yandex-oauth2');
+    return (
+        <Suspense fallback={<Loading />}>
+            <SocialAuthHandler />
+        </Suspense>
+    );
+}
 
-	return (
+function SocialAuthHandler() {
+    const [yandexAuthenticate] = useSocialAuthenticateMutation();
+    useSocialAuth(yandexAuthenticate, 'yandex-oauth2');
+
+    return (
+        <div className='my-8'>
+            <div className='flex justify-center my-8'>
+                <span className={'animate-spin'}><Loader/></span>
+            </div>
+        </div>
+    );
+}
+
+function Loading() {
+    return (
         <div className='my-8'>
             <div className='flex justify-center my-8'>
                 <span className={'animate-spin'}><Loader/></span>
